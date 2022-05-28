abcb3db1d55e0b37022fe0bbbf3a22f96ba58e68
parthvekariya98 <parthvekariya260898@gmail.com>
2022-05-27 22:37:08 -0400

    Main Screen, Details Screen, Wishlist screen(in progress)

---
+++ FP_Developers_iOS/FP_Developers_iOS/DetailImageCell.swift
@@ -0,0 +1,12 @@
+//
+//  DetailImageCell.swift
+//  FP_Developers_iOS
+//
+//  Created by parth on 2022-05-27.
+//
+
+import UIKit
+
+class DetailImageCell: UICollectionViewCell {
+    @IBOutlet weak var detail_image: UIImageView!
+}
---
+++ FP_Developers_iOS/FP_Developers_iOS/Helper.swift
@@ -0,0 +1,49 @@
+//
+//  Helper.swift
+//  FP_Developers_iOS
+//
+//  Created by parth on 2022-05-26.
+//
+import UIKit
+import Foundation
+
+extension UIView {
+
+    @IBInspectable var shadow: Bool {
+        get {
+            return layer.shadowOpacity > 0.0
+        }
+        set {
+            if newValue == true {
+                self.addShadow()
+            }
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return self.layer.cornerRadius
+        }
+        set {
+            self.layer.cornerRadius = newValue
+
+            // Don't touch the masksToBound property if a shadow is needed in addition to the cornerRadius
+            if shadow == false {
+                self.layer.masksToBounds = true
+            }
+        }
+    }
+    
+    func addShadow(shadowColor: CGColor = UIColor.black.cgColor,
+               shadowOffset: CGSize = CGSize(width: -1.0, height: 1.0),
+               shadowOpacity: Float = 0.4,
+               shadowRadius: CGFloat = 3.0) {
+        layer.shadowColor = shadowColor
+        layer.shadowOffset = shadowOffset
+        layer.shadowOpacity = shadowOpacity
+        layer.shadowRadius = shadowRadius
+        layer.masksToBounds = false
+    }
+}
+
+
