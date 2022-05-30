403bac7257f58c5cb7ce65c6ca82381182c837c6
parthvekariya98 <parthvekariya260898@gmail.com>
2022-05-30 10:22:19 -0400

    - video player for detail of place (with test video)

--- FP_Developers_iOS/FP_Developers_iOS/MainScreenController.swift
+++ FP_Developers_iOS/FP_Developers_iOS/MainScreenController.swift
@@ -8,16 +8,11 @@
 import UIKit
 import MapKit
 
-class MainScreenController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource ,CLLocationManagerDelegate ,UITextFieldDelegate{
+class MainScreenController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
    
 
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var mapView: MKMapView!
-    
-    @IBOutlet weak var textField_Address: UITextField!
-    //Location On Map
-    let locationManager = CLLocationManager()
-    var myGeoCoder = CLGeocoder()
     
     let test_places = ["Taj Mahal", "Eiffel Tower", "Canals of Amsterdam"]
     let test_places_images = ["place1", "place2", "place3"]
@@ -26,29 +21,6 @@ class MainScreenController: UIViewController, UICollectionViewDelegate, UICollec
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        mapView.showsUserLocation = false
-        mapView.isZoomEnabled = true
-        
-        locationManager.delegate = self
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-
-        locationManager.startUpdatingLocation()
-        // Coordinates on Map
-        let coordinates = CLLocationCoordinate2D(latitude: locationManager.location?.coordinate.latitude ?? 0.0, longitude:    locationManager.location?.coordinate.longitude ?? 0.0)
-
-        let span = MKCoordinateSpan (latitudeDelta: 0.5, longitudeDelta: 0.5)
-        let region = MKCoordinateRegion(center: coordinates, span: span)
-        mapView.setRegion(region, animated: true)
-        
-        // Annotation On Map
-        let myPin = MKPointAnnotation()
-        myPin.coordinate = coordinates
-        myPin.title = "Hi I'm Here"
-        myPin.subtitle = "Where Would you like to Go?"
-        mapView.addAnnotation(myPin)
-        
-        textField_Address.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -76,57 +48,6 @@ class MainScreenController: UIViewController, UICollectionViewDelegate, UICollec
         let DetailViewController = UIStoryboard(name: "Main", bundle:nil).instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
         self.navigationController?.pushViewController(DetailViewController, animated: true)
     }
-    //Action By the Search Button
-    @IBAction func searchBtn(_ sender: UIButton) {
-        myGeoCoder.geocodeAddressString(textField_Address.text ?? ""){
-            (placemarks,error) in
-            self.processResponse(withPlacemarks: placemarks, error: error)
-    }
-}
     
-    //Function for the Placemark on the Map
-    func processResponse(withPlacemarks placemarks:[CLPlacemark]?,error: Error?){
-        if let error = error{
-            print("error Fetching Coordinates (\(error)")
-        }else {
-            var location :CLLocation?
-            
-            if let placemarks = placemarks, placemarks.count > 0{
-                location = placemarks.first?.location
-            }
-            
-            if let location = location{
-                
-                let coordinate = location.coordinate
-                
-                
-                let request = MKDirections.Request()
-                request.source = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: locationManager.location?.coordinate.latitude ?? 0.0, longitude: locationManager.location?.coordinate.longitude ?? 0.0)))
-                request.destination = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)))
-                request.transportType = .any
-                request.requestsAlternateRoutes = true
-                                           
-                let directions = MKDirections(request: request)
-                directions.calculate{response , error in
-                guard let directionsresponse = response else {return}
-            
-                    for route in directionsresponse.routes{
-                        self.mapView.addOverlay(route.polyline)
-                        self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
-                    }
-                                           
-            }
-                // For the Annotation
-                let myPin = MKPointAnnotation()
-                
-                myPin.coordinate = coordinate
-                
-                myPin.title = textField_Address.text
-                mapView.addAnnotation(myPin)
-                
-        }
-        
-    }
-    }
-    
+
 }
