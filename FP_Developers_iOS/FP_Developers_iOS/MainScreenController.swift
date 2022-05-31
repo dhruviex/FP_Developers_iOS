f29efaf4f4714fd53a0d227a2dbd02ea1b197f94
dhruviex <C0846368@mylambton.ca>
2022-05-31 00:23:52 -0400

    Updated for Final Project
    Changes for the location

--- FP_Developers_iOS/FP_Developers_iOS/MainScreenController.swift
+++ FP_Developers_iOS/FP_Developers_iOS/MainScreenController.swift
@@ -8,19 +8,46 @@
 import UIKit
 import MapKit
 
-class MainScreenController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class MainScreenController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource,CLLocationManagerDelegate ,UITextFieldDelegate {
    
 
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var mapView: MKMapView!
     
+    @IBOutlet weak var textField_Address: UITextField!
     let test_places = ["Taj Mahal", "Eiffel Tower", "Canals of Amsterdam"]
     let test_places_images = ["place1", "place2", "place3"]
+    
+    let locationManager = CLLocationManager()
+    var myGeoCoder = CLGeocoder()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        mapView.showsUserLocation = false
+        mapView.isZoomEnabled = true
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+
+        locationManager.startUpdatingLocation()
+        
+        let coordinates = CLLocationCoordinate2D(latitude: locationManager.location?.coordinate.latitude ?? 0.0, longitude: locationManager.location?.coordinate.longitude ?? 0.0)
+
+        let span = MKCoordinateSpan (latitudeDelta: 0.5, longitudeDelta: 0.5)
+        let region = MKCoordinateRegion(center: coordinates, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        let myPin = MKPointAnnotation()
+        
+        myPin.coordinate = coordinates
+        myPin.title = "Hi I'm Here"
+        myPin.subtitle = "Where Would you like to Go?"
+        mapView.addAnnotation(myPin)
+        
+        textField_Address.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -32,6 +59,55 @@ class MainScreenController: UIViewController, UICollectionViewDelegate, UICollec
     }
     
 
+    @IBAction func searchBtn(_ sender: UIButton) {
+        myGeoCoder.geocodeAddressString(textField_Address.text ?? ""){
+            (placemarks,error) in
+            self.processResponse(withPlacemarks: placemarks, error: error)
+               }
+    }
+    func processResponse(withPlacemarks placemarks:[CLPlacemark]?,error: Error?){
+        if let error = error{
+            print("error Fetching Coordinates (\(error)")
+        }else {
+            var location :CLLocation?
+            
+            if let placemarks = placemarks, placemarks.count > 0{
+                location = placemarks.first?.location
+            }
+            
+            if let location = location{
+                
+                let coordinate = location.coordinate
+                
+                
+                let request = MKDirections.Request()
+                request.source = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: locationManager.location?.coordinate.latitude ?? 0.0, longitude: locationManager.location?.coordinate.longitude ?? 0.0)))
+                request.destination = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)))
+                request.transportType = .any
+                request.requestsAlternateRoutes = true
+                                           
+                let directions = MKDirections(request: request)
+                directions.calculate{response , error in
+                guard let directionsresponse = response else {return}
+            
+                    for route in directionsresponse.routes{
+                        self.mapView.addOverlay(route.polyline)
+                        self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
+                    }
+                                           
+            }
+                
+                let myPin = MKPointAnnotation()
+                
+                myPin.coordinate = coordinate
+                
+                myPin.title = textField_Address.text
+                mapView.addAnnotation(myPin)
+                
+           }
+        
+        }
+    }
     //collection view delegate methods
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return test_places.count
