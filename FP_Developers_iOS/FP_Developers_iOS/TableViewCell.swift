c5089a113aabad22f2c6fb79d9761c1bde29be32
deekshith024 <deekshithreddy0001@gmail.com>
2022-05-30 23:59:46 -0400

    3

---
+++ FP_Developers_iOS/FP_Developers_iOS/TableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  TableViewCell.swift
+//  FP_Developers_iOS
+//
+//  Created by Karthik Reddy Rondla on 2022-05-30.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    
+    
+    
+    @IBOutlet weak var lbl: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
