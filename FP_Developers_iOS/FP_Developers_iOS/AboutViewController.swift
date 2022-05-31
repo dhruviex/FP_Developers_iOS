660167768a58509ac1b8a00bb2a53327b304b2e6
alishadas664 <alishadas664@gmail.com>
2022-05-31 11:09:28 -0400

    - ui design for about us screen

---
+++ FP_Developers_iOS/FP_Developers_iOS/AboutViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AboutViewController.swift
+//  FP_Developers_iOS
+//
+//  Created by parth on 2022-05-31.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
