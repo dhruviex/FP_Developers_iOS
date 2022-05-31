d6bf45976af3f912ff9a3463411270879f9232fd
parthvekariya98 <parthvekariya260898@gmail.com>
2022-05-31 12:45:57 -0400

    - managed image picker for image selection and managed image view

--- FP_Developers_iOS/FP_Developers_iOS/CreateSiteController.swift
+++ FP_Developers_iOS/FP_Developers_iOS/CreateSiteController.swift
@@ -18,37 +18,57 @@ class CreateSiteController: UIViewController, UINavigationControllerDelegate, UI
     @IBOutlet weak var img3: UIImageView!
     
     var imagePicker = UIImagePickerController()
+    var currentImageIndex:Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //set image picker delegate
+        imagePicker.delegate = self
+        imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.allowsEditing = false
+
     }
     
-    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: [NSObject: AnyObject]){
-            self.dismiss(animated: true, completion: { () -> Void in
-
-            })
-        print("IMAGE------>",image as Any)
-        self.img1.image = image
-        self.btn1.isHidden = true
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            if (currentImageIndex == 0) {
+                img1.contentMode = .scaleToFill
+                img1.image = pickedImage
+                btn1.isHidden = true
+                currentImageIndex = 1
+            } else if (currentImageIndex == 1) {
+                img2.contentMode = .scaleToFill
+                img2.image = pickedImage
+                btn2.isHidden = true
+                currentImageIndex = 2
+            } else if (currentImageIndex == 2) {
+                img3.contentMode = .scaleToFill
+                img3.image = pickedImage
+                btn3.isHidden = true
+            }
+            
         }
+
+        dismiss(animated: true, completion: nil)
+    }
     
     @IBAction func firstBtnTap(_ sender: UIButton) {
+        //call image picker preset method
         if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
-                    print("Button capture")
-
-                    imagePicker.delegate = self
-                    imagePicker.sourceType = .savedPhotosAlbum
-                    imagePicker.allowsEditing = false
-
-                    present(imagePicker, animated: true, completion: nil)
-                }
+            present(imagePicker, animated: true, completion: nil)
+        }
     }
     
     @IBAction func secondBtnTap(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
+            present(imagePicker, animated: true, completion: nil)
+        }
     }
     
     @IBAction func thirdBtnTap(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
+            present(imagePicker, animated: true, completion: nil)
+        }
     }
 }
